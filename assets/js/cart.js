89ee12d951a48305d5cd8af74fd40354331ae80e
Ruben Soler <r.solerginer@gmail.com>
2016-10-10 20:10:01 +0200

    obtengo datos del producto

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -10,20 +10,13 @@
 //    
 //]
 var carrito = [];
-console.log(carrito.toString());
+
 function addToCart(div) {
     var id = div.id;
     var nombre = div.firstChild.nextSibling.firstChild.data;
     var precio = div.firstChild.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.firstChild.nextSibling.firstChild.nextSibling.firstChild.data;
     var img = div.firstChild.nextSibling.nextSibling.nextSibling.firstChild.nextSibling.src;
-    var producto = {
-        id: id,
-        nombre: nombre,
-        precio: precio,
-        img: img
-    }
-        this.carrito.push(producto);
-       console.log(this.carrito);
+    console.log("El producto es:  ID=" + id + " NOMBRE=" + nombre + " PRECIO=" + precio + " IMAGEN=" + img);
 }
 
 function subProduct(div) {
