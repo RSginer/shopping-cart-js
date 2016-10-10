aa6382965a115eeef31a0bd7e6453f3e04fe8818
Ruben Soler <r.solerginer@gmail.com>
2016-10-10 20:33:59 +0200

    añadir al carro

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -4,24 +4,30 @@ function addToCart(div) {
     var name = div.firstChild.nextSibling.firstChild.data;
     var price = div.firstChild.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.firstChild.nextSibling.firstChild.nextSibling.firstChild.data;
     var img = div.firstChild.nextSibling.nextSibling.nextSibling.firstChild.nextSibling.src;
-    var cartItem =
-            '<div class="cart-list__item__title">' +
-            '<p><b>'+ name + '</b> <i class="fa fa-minus-square"></i><i class="fa fa-plus-square"></i><i class="fa fa-trash"></i></p>' +
-            '</div>' +
-            ' <div class="cart-list__item__left">' +
-            ' <img class="cart-list__item__image" src="'+img+'"/>' +
-            ' </div>' +
-            '<div class="cart-list__item__info">' +
-            ' <p><b>Unidades:</b> <span class="cart-list__item__quantity">1</span></p>' +
-            ' <p><b>Precio:</b> <span class="cart-list__item__price">'+ price +'</span></p>' +
-            '</div>';
-    var node = document.createElement("div");
-    node.className="cart-list__item";
-    node.id=id;
-    node.innerHTML += cartItem;
-    document.getElementById("cartList").appendChild(node);
-    
-    
+
+    if (!this.isInCart(id)) {
+        var cartItem =
+                '<div class="cart-list__item__title">' +
+                '<p><b>' + name + '</b> <i class="fa fa-minus-square"></i><i class="fa fa-plus-square"></i><i class="fa fa-trash"></i></p>' +
+                '</div>' +
+                ' <div class="cart-list__item__left">' +
+                ' <img class="cart-list__item__image" src="' + img + '"/>' +
+                ' </div>' +
+                '<div class="cart-list__item__info">' +
+                ' <p><b>Unidades:</b> <span class="cart-list__item__quantity">1</span></p>' +
+                ' <p><b>Precio:</b> <span class="cart-list__item__price">' + price + '</span></p>' +
+                '</div>';
+        var node = document.createElement("div");
+        node.className = "cart-list__item";
+        node.id = id + "-cart";
+        node.innerHTML += cartItem;
+        document.getElementById("cartList").appendChild(node);
+    }else{
+        var nodeInCart = document.getElementById(id+"-cart");
+    }
+
+
+
     console.log("El producto es:  ID=" + id + " NOMBRE=" + name + " PRECIO=" + price + " IMAGEN=" + img);
 }
 
@@ -37,7 +43,7 @@ function addProduct(div) {
 
 }
 
-function isInCart(id){
-    
+function isInCart(id) {
+
     return false;
 }
