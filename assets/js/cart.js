c45dd9d7435d5d03f6deffc6c0b0a9c657a68228
Rubén Soler <r.solerginer@gmail.com>
2016-10-11 10:36:00 +0200

    borrar producto del carrito

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,3 +1,5 @@
+var totalPrice = 0;
+var cartCount = 0;
 function addToCart(div) {
     // DATOS DEL PRODUCTO
     var id = div.id;
@@ -9,7 +11,7 @@ function addToCart(div) {
         /* CREO EL ELEMENTO ITEM DE CARRITO*/
         var cartItem =
                 '<div class="cart-list__item__title">' +
-                '<p><b>' + name + '</b> <i class="fa fa-minus-square"></i><i class="fa fa-plus-square"></i><i class="fa fa-trash"></i></p>' +
+                '<p><b>' + name + '</b> <i class="fa fa-minus-square"></i><i class="fa fa-plus-square"></i><i class="fa fa-trash" onclick="deleteProduct(this)"></i></p>' +
                 '</div>' +
                 ' <div class="cart-list__item__left">' +
                 ' <img class="cart-list__item__image" src="' + img + '"/>' +
@@ -24,17 +26,17 @@ function addToCart(div) {
         node.innerHTML += cartItem;
         /* LO INSERTO EN EL DIV DE CARRITO*/
         document.getElementById("cart-list").insertBefore(node, document.getElementById("cart-list").firstChild);
-        updateTotal();
+        this.updateTotal(price);
     } else {
         var nodeInCart = document.getElementById(id + "-cart");
-        var nodeQuantity = nodeInCart.lastChild.firstChild.nextSibling.firstChild.nextSibling.nextSibling.firstChild;
+        var nodeQuantity = nodeInCart.getElementsByTagName("p")[1].lastChild.firstChild;
         var quantity = parseInt(nodeQuantity.data);
         quantity++;
         nodeQuantity.data = quantity;
-        var nodePrice = nodeInCart.lastChild.lastChild.firstChild.nextSibling.nextSibling.firstChild;
+        var nodePrice = nodeInCart.getElementsByTagName("p")[2].lastChild.firstChild;
         var priceChanged = parseInt(quantity) * price;
         nodePrice.data = parseInt(priceChanged);
-        updateTotal();
+        this.updateTotal(price);
     }
 }
 
@@ -43,7 +45,12 @@ function subProduct(div) {
 }
 
 function deleteProduct(div) {
-
+var cartList=document.getElementById("cart-list");
+var item = div.parentNode.parentNode.parentNode;
+var price = div.parentNode.parentNode.parentNode.getElementsByTagName("p")[2].lastChild.firstChild.data;
+console.log(price);
+cartList.removeChild(div.parentNode.parentNode.parentNode);
+this.updateTotal(-parseInt(price));
 }
 
 function addProduct(div) {
@@ -58,17 +65,9 @@ function isInCart(id) {
     }
 }
 
-function updateTotal() {
-    var nodeListSize = document.getElementById("cart-list").childNodes.length;
-    var totalPrice = 0;
-    for (var i = 0; i < nodeListSize; i++) {
-        var nodeInCart = document.getElementById("item" + i + "-cart");
-        if (nodeInCart != null) {
-            var nodePrice = nodeInCart.lastChild.lastChild.lastChild.firstChild.data;
-            console.log(nodePrice);
-            totalPrice += parseInt(nodePrice);
-        }
-    }
-    document.getElementById("total-price").innerHTML = totalPrice;
-    document.getElementById("cart-count-number").innerHTML = nodeListSize;
+function updateTotal(price) {
+    this.totalPrice+=parseInt(price);
+    this.cartCount+=1;
+    document.getElementById("total-price").innerHTML = this.totalPrice;
+    document.getElementById("cart-count-number").innerHTML = this.cartCount;
 }
